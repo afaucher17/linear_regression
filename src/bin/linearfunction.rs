95b74273745a0f38ba5f2ccaaf5ea297627fb131
afaucher17 <aymeric.faucher@gmail.com>
2016-05-16 18:16:24 +0200

    visualizedata.rs: csv parsed

--- src/bin/linearfunction.rs
+++ src/bin/linearfunction.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main () {
-  println!("{}", "linearfunction");
+  println!("Please enter a mileage: ");
+  let mut mileage = String::new();
+  io::stdin().read_line(&mut mileage).expect("failed to read the mileage");
+  let mileage: u32 = mileage.trim().parse().expect("The mileage you entered is not an interger.");
+  {
+      println!("{}", mileage);
+  }
 }
--- src/bin/visualizedata.rs
+++ src/bin/visualizedata.rs
@@ -1,3 +1,32 @@
+#[macro_use]
+extern crate clap;
+extern crate csv;
+
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
+fn read_file(filename: String) -> String {
+    let path = Path::new(&filename);
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Could not open {}: {}", display, Error::description(&why)),
+        Ok(file) => file,
+    };
+    let mut buffer = String::new();
+    match file.read_to_string(&mut buffer) {
+        Err(why) => panic!("Could not read {}: {}", display, Error::description(&why)),
+        Ok(_) => buffer,
+    }
+}
+
 fn main() {
-    println!("{}", "visualizedata");
+   let yaml = load_yaml!("cli.yml");
+   let options = clap::App::from_yaml(yaml).get_matches();
+   let data = read_file(String::from(options.value_of("file").unwrap()));
+   let mut rdr = csv::Reader::from_string(data);
+   for row in rdr.records().map(|r| r.unwrap()) {
+       println!("{:?}", row);
+   }
 }
