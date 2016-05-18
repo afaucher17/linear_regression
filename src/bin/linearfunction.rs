5673bb319d0e4a2939f2eee1e50fb16541cd856d
afaucher17 <aymeric.faucher@gmail.com>
2016-05-18 17:23:01 +0200

    over ?

--- src/bin/linearfunction.rs
+++ src/bin/linearfunction.rs
@@ -1,11 +1,22 @@
+#[macro_use]
+extern crate csv;
+
+type Row = (f64, f64);
+
 use std::io;
 
 fn main () {
-  println!("Please enter a mileage: ");
-  let mut mileage = String::new();
-  io::stdin().read_line(&mut mileage).expect("failed to read the mileage");
-  let mileage: u32 = mileage.trim().parse().expect("The mileage you entered is not an interger.");
-  {
-      println!("{}", mileage);
-  }
+    let (t0, t1, min, max) = if let Err(_) = std::fs::metadata("/tmp/afaucher/.tmp"){
+        (0, 0, 0, 0)
+    }
+    else {
+        let mut rdr = csv::Reader::from_file("/tmp/afaucher/.tmp").unwrap();
+        let rows = rdr.decode().collect::<csv::Result<Vec<Row>>>().unwrap();
+        (rows[0].0, rows[0].1, rows[1].0, rows[1].1)
+    };
+    println!("Please enter a mileage: ");
+    let mut mileage = String::new();
+    io::stdin().read_line(&mut mileage).expect("failed to read the mileage");
+    let mileage: u32 = mileage.trim().parse().expect("The mileage you entered is not an interger.");
+    println!("{}", t0 + (t1 * if (min != 0 && max != 0) { mileage - min / (max - min) } else { mileage }));
 }
--- src/bin/visualizedata.rs
+++ src/bin/visualizedata.rs
@@ -9,17 +9,19 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
 
-fn read_file(filename: String) -> String {
-    let path = Path::new(&filename);
+fn write_to_file(result: &String, filename: &String)
+{
+    let path = Path::new(filename);
     let display = path.display();
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("Could not open {}: {}", display, Error::description(&why)),
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("Could not create {}: {}", display, Error::description(&why)),
         Ok(file) => file,
     };
-    let mut buffer = String::new();
-    match file.read_to_string(&mut buffer) {
-        Err(why) => panic!("Could not read {}: {}", display, Error::description(&why)),
-        Ok(_) => buffer,
+    if let Err(_) = std::fs::metadata(path.parent().unwrap()){
+        std::fs::create_dir(path.parent().unwrap()).unwrap()
+    }
+    if let Err(why) = file.write_all(result.as_bytes()) {
+        panic!("Could not write to {}: {}", display, Error::description(&why));
     }
 }
 
@@ -28,21 +30,17 @@ fn linear_regression(input_file: String)
     let mut rdr = csv::Reader::from_file(input_file).unwrap();
     let rows = rdr.decode().collect::<csv::Result<Vec<Row>>>().unwrap();
     let lr: f64 = 0.07;
-    let min = rows.iter().map(|&(km, _)| km as i32).min().unwrap();
-    let max = rows.iter().map(|&(km, _)| km as i32).max().unwrap();
-    let diff = (max - min) as f64;
-    let rescale = |x| (x - min as f64) / diff;
-    let estimate_price = |mileage, (x, y)| x + (y * rescale(mileage));
-    let (t0, t1) = (0..3000).fold((0.0, 0.0), |(x, y), _| {
-        let res: (f64, f64) = rows.iter().fold((x, y), | _, &(km, price)| {
-            (estimate_price(km, (x, y)) - price, (estimate_price(km, (x, y)) - price) * rescale(km))
+    let min = rows.iter().map(|&(km, _)| km as i32).min().unwrap() as f64;
+    let max = rows.iter().map(|&(km, _)| km as i32).max().unwrap() as f64;
+    let rescale = |val| (val - min) / (max - min);
+    let estimate_price = |mileage, (x, y)| x + (y * mileage);
+    let (t0, t1) = (0..60000).fold((0.0, 0.0), |(x, y), _| {
+        let res: (f64, f64) = rows.iter().fold((0.0, 0.0), |(x1, x2), &(km, price)| {
+            (x1 + (estimate_price(rescale(km), (x, y)) - price), x2 + ((estimate_price(rescale(km), (x, y)) - price) * rescale(km)))
         });
         (x - (res.0 / rows.len() as f64 * lr), y - (res.1 / rows.len() as f64 * lr))
     });
-    for &(km, price) in rows.iter()
-    {
-        println!("{} {}", price, estimate_price(km, (t0, t1)));
-    }
+    write_to_file(&format!("{},{}\n{},{}", t0, t1, min, max), &String::from("/tmp/afaucher/.tmp"));
 }
 
 fn main() {
